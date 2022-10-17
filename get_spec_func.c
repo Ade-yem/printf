1949fdc9994f6c54a3b66ecf07d5341537d88f93
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-10-17 12:58:05 -0700

    edited functions

--- get_spec_func.c
+++ get_spec_func.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <stddef.h>
 
 /**
  * get_spec_func - selects the correct function to perform the specifier
@@ -12,28 +10,52 @@
 
 int (*get_spec_func(const char *s, int index))(va_list, char *, unsigned int)
 {
-	print_t formats[] = {
-		{"c", handle_char},
-		{"s", handle_str},
-		{"d", handle_int},
-		{"i", handle_int},
-		{NULL, NULL}
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str},
+		{"i", print_int}, {"d", print_int},
+		{"b", print_bnr},/* {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex},
+		{"X", print_upx}, {"S", print_usr},
+		{"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt},
+		{"lo", prinloct}, {"lx", prinlhex},
+		{"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt},
+		{"ho", prinhoct}, {"hx", prinhhex},
+		{"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx},
+		{"#i", print_int}, {"#d", print_int},
+		{"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt},
+		{"+o", print_oct}, {"+x", print_hex},
+		{"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt},
+		{" o", print_oct}, {" x", print_hex},
+		{" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg},
+		{"l", print_prg}, {"h", print_prg},
+		{" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint},
+		{" %", print_prg}, */{NULL, NULL},
 	};
-	int i = 0, j = 0, f_ind;
+	int i = 0, j = 0, first_index;
 
-	f_ind = index;
-
-	while (formats[i].type_arg != NULL)
+	first_index = index;
+	while (pr[i].type_arg)
 	{
-		if (s[index] == formats[i].type_arg[j])
+		if (s[index] == pr[i].type_arg[j])
 		{
-			if (formats[i].type_arg[j + 1] != '\0')
+			if (pr[i].type_arg[j + 1] != '\0')
 				index++, j++;
 			else
 				break;
 		}
 		else
-			j = 0, i++, index = f_ind;
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
-	return (formats[i].f);
+	return (pr[i].f);
 }

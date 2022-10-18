0264000854bb41753f6683cdfe97a4611f4dd4b8
Adejumo Adeyemi <93265896+Ade-yem@users.noreply.github.com>
2022-10-18 06:19:48 +0100

    Update ev_print_func.c

--- ev_print_func.c
+++ ev_print_func.c
@@ -9,7 +9,7 @@ int ev_print_func(const char *s, int index)
 {
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str}, {"i", print_int},
-		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
+		{"d", print_int}, {"b", print_bnr},/* {"u", print_unt},
 		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
 		{"S", print_usr}, {"p", print_add}, {"li", prinlint},
 		{"ld", prinlint}, {"lu", prinlunt}, {"lo", prinloct},
@@ -24,7 +24,7 @@ int ev_print_func(const char *s, int index)
 		{" x", print_hex}, {" X", print_upx}, {"R", print_rot},
 		{"r", print_rev}, {"%", print_prg}, {"l", print_prg},
 		{"h", print_prg}, {" +i", prinpint}, {" +d", prinpint},
-		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
+		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},*/
 		{NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
